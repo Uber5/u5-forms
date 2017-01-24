318faca811be45dd58091d87f544eee47e128cbc
Chris Oloff <christian.oloff@gmail.com>
2017-01-24 17:53:34 +0200

    component testing using enzyme, not that great so far

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,11 +1,40 @@
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import React from 'react'
-import { createForm } from '../'
+import RaisedButton from 'material-ui/RaisedButton'
+
+import { createForm, createAddForm } from '../'
+
+// Should go to some SETUP
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const mountWithContext = (node) => mount(node, {
+  context: {
+    muiTheme: getMuiTheme(),
+  },
+  childContextTypes: {
+    muiTheme: React.PropTypes.object.isRequired,
+  }
+});
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+// END SETUP
+
 
 describe('createForm', () => {
   it('does the dummy thing', () => {
     const Form = createForm()
     const rendered = shallow(<Form />)
-    expect(rendered.text()).toEqual('This is not a form yet')
+    expect(rendered.text()).toMatch(/This is not/)
+  })
+})
+
+describe('createAddForm', () => {
+  it('renders a submit button with the label provided', () => {
+    const labelText = 'Submit it, now...'
+    const Form = createAddForm({ submitLabel: labelText })
+    const wrapper = mountWithContext(<Form />)
+    console.log('wrapper.html', wrapper.html())
+    const button = wrapper.find(RaisedButton)
+    expect(button.label).toBe(labelText)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export const createForm = () => () => <p>This is not a form yet</p>
+
+export const createAddForm = () => ({ submitLabel }) => (<form>
+  <RaisedButton label={submitLabel}>
+    huh
+  </RaisedButton>
+</form>)
