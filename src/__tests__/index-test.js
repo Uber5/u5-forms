50ed797f8d101740e2e7e5ae49c67f3cfd35346a
Chris Oloff <christian.oloff@gmail.com>
2017-01-24 22:15:01 +0200

    global test setup, more sample, wip

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -3,17 +3,6 @@ import { shallow, mount } from 'enzyme' // TODO: still want to use it?
 
 import { createForm, createAddForm } from '../'
 
-import renderer from 'react-test-renderer'
-
-// TODO: should be in *another* place
-// import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-const render = component => {
-  const wrapped = <MuiThemeProvider>{ component }</MuiThemeProvider>
-  return renderer.create(wrapped)
-}
-// END TODO
-
 describe('createForm', () => {
   it('does the dummy thing', () => {
     const Form = createForm()
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton'
 export const createForm = () => () => <p>This is not a form yet</p>
 
 export const createAddForm = () => ({ submitLabel }) => (<form>
-  <RaisedButton label={submitLabel}>
-    huh (where is the label)
+  <RaisedButton>
+    {submitLabel || 'submit'}
   </RaisedButton>
 </form>)
