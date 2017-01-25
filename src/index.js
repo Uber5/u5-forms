d677248a095a49f21c0c2478d6fb245abf7381fd
Chris Oloff <christian.oloff@gmail.com>
2017-01-25 17:30:42 +0200

    extract componentForFields() into own file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
 
-export const createAddForm = ({ submitLabel = 'submit' }) => () => (<form>
+import componentForFields from './component-for-fields'
+
+const createAddForm = ({ submitLabel = 'submit' }) => () => (<form>
   <RaisedButton label={submitLabel}>
   </RaisedButton>
 </form>)
 
-export const componentForFields = (fields) => () => <p>components</p>
+export { componentForFields, createAddForm }
